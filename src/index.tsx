dd444375e74118e9d75e21e1745e366f149a4969
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2025-02-27 21:15:42 +0000

    initial

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Hello from './Hello';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Hello/>
-  </React.StrictMode>
-);
